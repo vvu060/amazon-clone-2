e201920f7e1115e965c3a835287eedadca79a34e
VishalUrankar-2211 <vvu060@gmail.com>
2021-05-21 11:03:28 +0530

    home page done

---
+++ src/components/Banner.js
@@ -0,0 +1,44 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+
+function Banner() {
+  return (
+    <div className="relative">
+      <div className="absolute w-full h-32 bg-gradient-to-t from-gray-100 to-transparent bottom-0 z-20" />
+      <Carousel
+        autoplay={true}
+        infiniteLoop
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        interval={2000}
+      >
+        <div>
+          <img
+            loading="lazy"
+            src="https://links.papareact.com/gi1"
+            alt="Banner Image 1"
+          />
+        </div>
+
+        <div>
+          <img
+            loading="lazy"
+            src="https://links.papareact.com/6ff"
+            alt="Banner Image 2"
+          />
+        </div>
+
+        <div>
+          <img
+            loading="lazy"
+            src="https://links.papareact.com/7ma"
+            alt="Banner Image 3"
+          />
+        </div>
+      </Carousel>
+    </div>
+  );
+}
+
+export default Banner;
