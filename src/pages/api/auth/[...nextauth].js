70a8281ee89271d689d202d8f814c15f031cb66e
VishalUrankar-2211 <vvu060@gmail.com>
2021-05-22 10:50:28 +0530

    facebook sign in added

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -8,5 +8,9 @@ export default NextAuth({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
+    Providers.Facebook({
+      clientId: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
+    }),
   ],
 });
